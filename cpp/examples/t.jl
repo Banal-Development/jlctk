492cd8d3fc41e491e7c387929e98589a584189ac
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2024-01-20 08:38:20 -0500

    code struct improvements

--- cpp/examples/t.jl
+++ cpp/examples/t.jl
@@ -20,6 +20,12 @@ function open_mmap(fn::String)::Ptr{Cvoid}
   return ptr
 end
 
+struct RingDescriptor
+  ring_size::UInt64
+  end_idx::UInt64
+  #planet_pos_v::Vector{PlanetPos}
+end
+
 struct PlanetPos
   planet_num::Int64
   t::Float64
@@ -28,32 +34,25 @@ struct PlanetPos
   planet::SVector{16, UInt8}
 end
 
-struct PlanetPosRing
-  ring_size::UInt64
-  end_idx::UInt64
-  #planet_pos_v::Vector{PlanetPos}
-end
-
 function main()
  mmap_v_ptr = open_mmap("./out_seg.bin")
  # from https://stackoverflow.com/a/40246607/1181482
- mmap_ptr = reinterpret(Ptr{PlanetPosRing}, mmap_v_ptr)
- mmap_o = unsafe_wrap(Vector{PlanetPosRing}, mmap_ptr, 1)
- ring_size = mmap_o[1].ring_size
- println("ring_size: ", ring_size)
- println("end_idx: ", mmap_o[1].end_idx) 
- println(mmap_v_ptr, " ", mmap_v_ptr + sizeof(PlanetPosRing))
- println(mmap_ptr, " ", mmap_ptr + sizeof(PlanetPosRing))
+ mmap_ptr = reinterpret(Ptr{RingDescriptor}, mmap_v_ptr)
+ ring_o = view(unsafe_wrap(Vector{RingDescriptor}, mmap_ptr, 1), 1)[1]
+ planet_pos_v_addr = reinterpret(Ptr{PlanetPos}, mmap_v_ptr + sizeof(RingDescriptor))
+ planet_pos_v = unsafe_wrap(Vector{PlanetPos}, planet_pos_v_addr, ring_o.ring_size)
 
- planet_pos_v_addr = reinterpret(Ptr{PlanetPos}, mmap_v_ptr + sizeof(PlanetPosRing))
- planet_pos_v = unsafe_wrap(Vector{PlanetPos}, planet_pos_v_addr, ring_size)
+ println("ring_size: ", ring_o.ring_size)
+ println("end_idx: ", ring_o.end_idx) 
+ println(mmap_v_ptr, " ", mmap_v_ptr + sizeof(RingDescriptor))
+ println(mmap_ptr, " ", mmap_ptr + sizeof(RingDescriptor))
 
  start_idx = 1
  while true
-  last_idx = mmap_o[1].end_idx-1
+  last_idx = ring_o.end_idx-1
   if last_idx >= start_idx
-     println(planet_pos_v[mod(start_idx, ring_size)], " --> ", planet_pos_v[mod(last_idx, ring_size)])
-     println(String(planet_pos_v[mod(start_idx, ring_size)].planet))
+     println(planet_pos_v[mod(start_idx, ring_o.ring_size)], " --> ", planet_pos_v[mod(last_idx, ring_o.ring_size)])
+     println(String(planet_pos_v[mod(start_idx, ring_o.ring_size)].planet))
   end
   #for i in start_idx:last_idx
   #  println(planet_pos_v[i])
