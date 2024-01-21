1f21d4a5fd98665f4ed0f6f64889fe4840ad513a
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2024-01-20 20:12:17 -0500

    fixed julia consumer in planets example

--- examples/planets/t.jl
+++ examples/planets/t.jl
@@ -38,21 +38,21 @@ function main()
  mmap_v_ptr = open_mmap("./out_seg.bin")
  # from https://stackoverflow.com/a/40246607/1181482
  mmap_ptr = reinterpret(Ptr{RingDescriptor}, mmap_v_ptr)
- ring_o = view(unsafe_wrap(Vector{RingDescriptor}, mmap_ptr, 1), 1)[1]
+ ring_o = unsafe_wrap(Vector{RingDescriptor}, mmap_ptr, 1)
  planet_pos_v_addr = reinterpret(Ptr{PlanetPos}, mmap_v_ptr + sizeof(RingDescriptor))
- planet_pos_v = unsafe_wrap(Vector{PlanetPos}, planet_pos_v_addr, ring_o.ring_size)
+ planet_pos_v = unsafe_wrap(Vector{PlanetPos}, planet_pos_v_addr, ring_o[1].ring_size)
 
- println("ring_size: ", ring_o.ring_size)
- println("end_idx: ", ring_o.end_idx) 
+ println("ring_size: ", ring_o[1].ring_size)
+ println("end_idx: ", ring_o[1].end_idx) 
  println(mmap_v_ptr, " ", mmap_v_ptr + sizeof(RingDescriptor))
  println(mmap_ptr, " ", mmap_ptr + sizeof(RingDescriptor))
 
  start_idx = 1
  while true
-  last_idx = ring_o.end_idx-1
+  last_idx = ring_o[1].end_idx-1
   if last_idx >= start_idx
-     println(planet_pos_v[mod(start_idx, ring_o.ring_size)], " --> ", planet_pos_v[mod(last_idx, ring_o.ring_size)])
-     println(String(planet_pos_v[mod(start_idx, ring_o.ring_size)].planet))
+     println(planet_pos_v[mod(start_idx, ring_o[1].ring_size)], " --> ", planet_pos_v[mod(last_idx, ring_o[1].ring_size)])
+     println(String(planet_pos_v[mod(start_idx, ring_o[1].ring_size)].planet))
   end
   #for i in start_idx:last_idx
   #  println(planet_pos_v[i])
